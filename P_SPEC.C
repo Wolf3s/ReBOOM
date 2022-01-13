20ab698e5e1e36d68b68b312a35bbe2f5e96409f
atsb <atsb@atsb>
2022-01-13 07:36:26 +0100

    Adding secret sound and message

--- P_SPEC.C
+++ P_SPEC.C
@@ -2190,6 +2190,8 @@ void P_PlayerInSpecialSector (player_t* player)
 
       case 9:
         // Tally player in secret sector, clear secret special
+        player->message = FOUNDASECRET;
+        S_StartSound(player->mo,sfx_radio);
         player->secretcount++;
         sector->special = 0;
         break;
